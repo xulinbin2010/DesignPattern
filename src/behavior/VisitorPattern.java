c5e452f76d79de778bf8b8061e083ace1142569b
xulinbin <linbing.xu@carrier.com>
2024-04-24 09:12:11 +0800

    根据以上git diff信息生成的commit message如下：
    "移除了VisitorPattern.java中的detach方法，优化了代码结构。"

--- src/behavior/VisitorPattern.java
+++ src/behavior/VisitorPattern.java
@@ -78,10 +78,6 @@ class ObjectStructure {
         elements.add(element);
     }
 
-    public void detach(Element element) {
-        elements.remove(element);
-    }
-
     public void accept(Visitor visitor) {
         for (Element element : elements) {
             element.accept(visitor);
